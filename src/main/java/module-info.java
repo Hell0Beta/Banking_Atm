d115dc762429f6a8b337a250b7b33fa868b0dda6
Hell0Beta <welcome.tedlee@gmail.com>
2025-03-29 02:37:14 -0700

    *Created data.json which will act as our db *readjson() - to read all data from the json file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.banking_atm {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.example.banking_atm to javafx.fxml;
