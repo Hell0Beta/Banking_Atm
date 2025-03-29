5643a90c39b464a480256609c3aab65060d959b7
Hell0Beta <welcome.tedlee@gmail.com>
2025-03-29 09:46:14 -0700

    Added fun to delete create fetch one user and all users

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.banking_atm {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
+    requires java.desktop;
 
 
     opens com.example.banking_atm to javafx.fxml;
