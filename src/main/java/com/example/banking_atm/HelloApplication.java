85bf005079f5d34245676fc8cc2fa3426f5004b0
Hell0Beta <welcome.tedlee@gmail.com>
2025-04-02 05:19:56 -0700

    Rearranged directory, pls notify if any problems

--- src/main/java/com/example/banking_atm/HelloApplication.java
+++ src/main/java/com/example/banking_atm/HelloApplication.java
@@ -13,29 +13,13 @@ import java.util.Map;
 import java.io.File;
 // Now our code has been push to git lets push to github
 public class HelloApplication extends Application {
-    public static Map<String, Object> readjson() throws IOException{
-//      This line creates a ObjectMapper object - This allows us to convert a json object to a hash map
-        ObjectMapper objectMapper = new ObjectMapper();
-
-
-//      hashmap(key:str,val:Obj) var  objectmapper reads value from ----the---`data.json` file, further specifies its a hash map
-        Map<String, Object> data = objectMapper.readValue(new File("data.json"), Map.class) ;
-
-//      Prints info to console
-        System.out.println("Read JSON: " + data);
-
-        return data;
-    }
-
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 603, 474);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-//        Calls read.json
-        readjson();
         stage.show();
     }
 
--- src/main/java/com/example/banking_atm/loginController.java
+++ src/main/java/com/example/banking_atm/loginController.java
@@ -1,4 +1,41 @@
 package com.example.banking_atm;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class loginController {
+
+    @FXML
+    private Button forgotpswrdbtn;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button signup;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void onForgot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogin(ActionEvent event) {
+        System.out.println("lplp");
+    }
+
+    @FXML
+    void onSignup(ActionEvent event) {
+
+    }
+
 }
