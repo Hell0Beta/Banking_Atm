cae2b40e0765988b3c96a10bc61f824cd94c9389
kwabenatheo <ce-ttawiah0923@st.umat.edu.gh>
2025-04-05 17:41:58 +0100

    This is my commit message

---
+++ src/main/java/com/example/banking_atm/SignupController.java
@@ -0,0 +1,34 @@
+package com.example.banking_atm;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class SignupController {
+
+    @FXML
+    private ProgressBar passwordStrengthBar;
+
+    @FXML
+    private Label passwordStrengthLabel;
+
+    @FXML
+    private Button signinButton;
+
+    @FXML
+    private Button signupButton;
+
+    @FXML
+    void handleSignin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSignup(ActionEvent event) {
+
+    }
+
+}
