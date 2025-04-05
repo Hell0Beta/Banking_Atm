fe91f07b2e8c529e4bb34d8774597ab62f8fffa9
Afealete <doeemmanuel948@gmail.com>
2025-04-05 04:44:15 -0700

    Update ForgotPin.java
    added the methods to the ForgotPin controller class

--- src/main/java/com/example/banking_atm/ForgotPin.java
+++ src/main/java/com/example/banking_atm/ForgotPin.java
@@ -1,5 +1,30 @@
 package com.example.banking_atm;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
 
 public class ForgotPin {
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private Hyperlink lnkBackToLogin;
+
+    @FXML
+    private TextField txtEmailorPhone;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onReset(ActionEvent event) {
+
+    }
+
 }
