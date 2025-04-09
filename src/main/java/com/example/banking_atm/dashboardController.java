63895cc63f2199e9904aee89af9f2c588cfd22c4
nanaabenak <nanaabenabk@gmail.com>
2025-04-09 22:40:42 +0000

    dashboard

---
+++ src/main/java/com/example/banking_atm/dashboardController.java
@@ -0,0 +1,76 @@
+package com.example.banking_atm;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class dashboardController {
+
+    @FXML
+    private Button btnDeposit;
+
+    @FXML
+    private Button btnViewFinances;
+
+    @FXML
+    private Button btnWithdraw;
+
+    @FXML
+    private ImageView btndashboardHome;
+
+    @FXML
+    private ImageView btnimgLogout;
+
+    @FXML
+    private ImageView btnimgViewFinances;
+
+    @FXML
+    private Label lblBalance;
+
+    @FXML
+    private Label lblDebtToIncomeRatio;
+
+    @FXML
+    private Text lblEmergencyfund;
+
+    @FXML
+    private Text lblTotalValueDeposited;
+
+    @FXML
+    private Label lblWithdrawal;
+
+    @FXML
+    private Text txtWelcome;
+
+    @FXML
+    void Logout(MouseEvent event) {
+
+
+
+    }
+
+    @FXML
+    void onDeposit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewFinances(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onWithdraw(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openDashboard(MouseEvent event) {
+
+    }
+
+}
